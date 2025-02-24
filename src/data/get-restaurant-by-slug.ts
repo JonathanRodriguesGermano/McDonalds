3d703e2317ef28745d7a0fe4f80a7b23a606f440
jonathan <jonathanrodriguesgermano7@gmail.com>
2025-02-24 01:50:57 -0300

    feate: add restaurant menu page with categories and products

--- src/data/get-restaurant-by-slug.ts
+++ src/data/get-restaurant-by-slug.ts
@@ -3,4 +3,4 @@ import { db } from "@/lib/prisma";
 export const getRestaurantBySlug = async (slug: string) => {
     const restaurant = await db.restaurant.findUnique({ where: { slug } });
     return restaurant;
-}
+};
